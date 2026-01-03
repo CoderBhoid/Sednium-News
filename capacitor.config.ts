d479ae828d0ae197ff793754dfd14f9f2e507d0d
Sednium Builder <builder@sednium.com>
2026-01-03 16:37:54 +0530

    Fix: Rename N-News to Sednium-News and fix gradlew permissions

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.sednium.news',
   appName: 'Sednium News',
-  webDir: 'N-News'
+  webDir: 'Sednium-News'
 };
 
 export default config;
